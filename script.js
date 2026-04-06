ac4cf74091609f5451d5c63096f5ffe6cb1239d5
Anisiya <n1sp1x@bk.ru>
2026-04-06 20:47:38 +0300

    Finish werk; 1st extra credit

--- script.js
+++ script.js
@@ -2,15 +2,11 @@ let container = document.querySelector('.container')
 let button = document.createElement('button');
 let number;
 let body = document.querySelector('body');
-let blackContainer = document.createElement('div');
 body.style.backgroundColor = 'rgba(169, 198, 224, 1)';
 document.body.prepend(button);
 
 button.textContent = 'NEW ART'
 button.setAttribute('style','color:red;background-color:yellow; font-size:60px;position:absolute;margin-left:40px;border-radius:25px;border:2px solid orange; padding:15px');
-
-blackContainer.setAttribute('style', 'width: 1000px;height: 900px;background-color:black; left: 50%;transform: translateX(-50%);position:absolute; opacity: 0; pointer-events: none;');
-document.body.prepend(blackContainer)
 
 //Default Grid
 for (i = 0; i<256; i ++){
@@ -37,8 +33,7 @@ function newGrid(){
             allDivs.style.width = (100 / number) + '%' 
             container.appendChild(allDivs);
             allDivs.addEventListener('mouseenter', () => {
-                allDivs.style.backgroundColor = (Math.random().toString(16).slice(-6))
-                blackContainer.style.opacity += 0.1
+                allDivs.style.backgroundColor = '#' + Math.random().toString(16).substr(-6);
             })
         };
     }
